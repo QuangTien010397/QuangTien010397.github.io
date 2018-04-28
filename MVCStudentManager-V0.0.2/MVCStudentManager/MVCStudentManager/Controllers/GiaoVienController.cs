498233c9e68347afdc5f8cf08d8b93f159d7eb73
TranTanPhat210897 <15DH110265TTP@gmail.com>
2018-04-28 22:17:13 +0700

    MVCStudent-v0.02

---
+++ MVCStudentManager-V0.0.2/MVCStudentManager/MVCStudentManager/Controllers/GiaoVienController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCStudentManager.Models;
+using PagedList;
+using PagedList.Mvc;
+using System.IO;
+
+namespace MVCStudentManager.Controllers
+{
+    public class GiaoVienController : Controller
+    {
+        dbStudentManagerDataContext db = new dbStudentManagerDataContext();
+        
+        // GET: GiaoVien
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Trang đăng nhập
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection collection)
+        {
+            var tendn = collection["username"];
+            var matkhau = collection["password"];
+            if (String.IsNullOrEmpty(tendn))
+            {
+                ViewData["Loi1"] = "Phải nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi2"] = "Phải nhập mật khẩu";
+            }
+            else
+            {
+                Admin ad = db.Admins.SingleOrDefault(n => n.UserAdmin == tendn && n.PassAdmin == matkhau);
+                if (ad != null)
+                {
+                    Session["Taikhoanadmin"] = ad;
+                    return RedirectToAction("Index", "GiaoVien");
+                }
+                else
+                    ViewBag.ThongBao = "Tên đăng nhập hoặc mật khẩu không đúng";
+            }
+            return View();
+        }
+    }
+}
