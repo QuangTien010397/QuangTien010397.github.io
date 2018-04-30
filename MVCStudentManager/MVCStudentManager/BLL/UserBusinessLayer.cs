edd69580626bde29748c3512bc4e01ecbe7f7b3b
QuangTIen <quangtien010397@gmail.com>
2018-04-30 19:22:26 +0700

    MVC

---
+++ MVCStudentManager/MVCStudentManager/BLL/UserBusinessLayer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVCStudentManager.Models;
+
+namespace MVCStudentManager.BLL
+{
+    public class UserBusinessLayer
+    {
+        public UserStatus GetUserValidity(UserDetails user)
+        {
+            if (user.UserName == "Admin" && user.Password == "123")
+            {
+                return UserStatus.AuthenticatedAdmin;
+            }
+            else if (user.UserName == "SV" && user.Password == "SV")
+            {
+                return UserStatus.AuthenticatedUser;
+            }
+            else
+            {
+                return UserStatus.NonAuthenticatedUser;
+            }
+        }
+    }
+}
